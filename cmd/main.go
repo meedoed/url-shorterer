07ec2113e1b071c113a0a1388e1dadac6fdb2904
meedoed <bakkipooh@gmail.com>
2022-11-19 22:01:32 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/meedoed/url-shorterer/internal/config"
 	"github.com/meedoed/url-shorterer/internal/user"
 	"github.com/meedoed/url-shorterer/pkg/logging"
 )
 
 func main() {
 	logger := logging.GetLogger()
-
-	logger.Info("start app")
+	logger.Info("create app")
 	app := fiber.New()
+
+	cfg := config.GetConfig()
 
 	handler := user.NewHandler(logger)
 	handler.Register(app)
 
-	start(app)
+	start(app, cfg)
 }
 
-func start(app *fiber.App) {
+func start(app *fiber.App, cfg *config.Config) {
 	logger := logging.GetLogger()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello from fiber!")
 	})
 
-	logger.Info("Server is listening...")
-	logger.Fatal(app.Listen(":8000"))
+	logger.Infof("Server is listening port %s:%s", cfg.Listen.BindIp, cfg.Listen.Port)
+	logger.Fatal(app.Listen(fmt.Sprintf("%s:%s", cfg.Listen.BindIp, cfg.Listen.Port)))
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/meedoed/url-shorterer
 go 1.19
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/gofiber/fiber/v2 v2.39.0 // indirect
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/ilyakaznacheev/cleanenv v1.4.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -15,4 +19,6 @@ require (
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
